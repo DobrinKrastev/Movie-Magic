420e4812a7f458ea184a840b81a008a8eaf681e5
DobrinKrastev <152500080+DobrinKrastev@users.noreply.github.com>
2024-01-29 00:19:51 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const express = require("express");
+
+
+const app = express();
+const port = 5000;
+
+app.get("/",(req,res)=>{
+res.send("Home page");
+
+})
+
+app.listen(port,()=> console.log(`App is listening on port ${port}`));
