bf6030b99d46f7e65a9d2ccd5aec580c7d2b9da6
DobrinKrastev <152500080+DobrinKrastev@users.noreply.github.com>
2024-01-29 12:39:19 +0200

    Add 404 page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,5 +7,11 @@ router.get("/",(req,res)=>{
 
     router.get("/about",(req,res)=>{
         res.render("about")
-    })
+    });
+
+    router.get("*",(req,res)=>{
+        res.render("404")
+    });
+
+    
     module.exports = router;
--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const homeController = require("./controllers/homeController");
-router.use(homeController);
+
 
 const movieController = require("./controllers/movieController");
+
 router.use(movieController)
+router.use(homeController);
 
     module.exports = router;
