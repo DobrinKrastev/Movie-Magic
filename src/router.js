50861ac7a3eba8311b936c1dfe8b8398e9221450
DobrinKrastev <152500080+DobrinKrastev@users.noreply.github.com>
2024-01-29 12:32:42 +0200

    Add create movie page

--- src/router.js
+++ src/router.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const homeController = require("./controllers/homeController");
 router.use(homeController);
 
+const movieController = require("./controllers/movieController");
+router.use(movieController)
+
     module.exports = router;
