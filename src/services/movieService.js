87ada207551753635b53309eaa66758ed94b47e1
DobrinKrastev <152500080+DobrinKrastev@users.noreply.github.com>
2024-01-30 11:43:18 +0200

    Add "hard-coded" movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,16 @@
+movies=[
+    { title: 'Jungle Cuise',
+      genre: 'Adventure',
+      director: 'Dweyne Johnson',
+      date: '2020',
+      imgUrl: '/img/jungle-cruise.jpeg',
+      rate: '5',
+      description: 'Dreaming about saving countless lives and having another adventure'
+    }
+]
+
 function createMovie(movieData){
- console.log(movieData);
+ movies.push(movieData);
 
 
 }
