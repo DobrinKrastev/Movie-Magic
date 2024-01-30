5cdea1ef372d805959f95bb4aeed7b53153b942f
DobrinKrastev <152500080+DobrinKrastev@users.noreply.github.com>
2024-01-30 11:29:36 +0200

    Add body-parser

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -4,8 +4,8 @@ const path = require("path");
 function expressConfig(app){
 
     app.use(express.static(path.resolve("src/public")));
-
-    return app
+    app.use(express.urlencoded({extended:false}));
+    return app;
 
 }
 
