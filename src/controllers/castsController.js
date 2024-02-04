25f6db07562b5df753329b4200ec0265072e48c6
DobrinKrastev <152500080+DobrinKrastev@users.noreply.github.com>
2024-02-04 15:12:38 +0200

    Add post action in casts page

--- src/controllers/castsController.js
+++ src/controllers/castsController.js
@@ -5,4 +5,11 @@ router.get("/cast/create",(req,res)=>{
 res.render("casts/cast-create");
 });
 
+router.post("/cast/create", (req,res)=>{
+
+    const cast = req.body;
+    console.log(cast);
+    res.redirect("/")
+})
+
 module.exports = router;
