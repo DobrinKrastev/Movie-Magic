cafa57405ffaa8ce5108fc0cb5c0e8b927e3a561
DobrinKrastev <152500080+DobrinKrastev@users.noreply.github.com>
2024-02-08 11:22:05 +0200

    Upload user model and add register to DB

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
-
+const authService = require("../services/authService")
 
 router.get("/register", (req,res)=>{
     res.render("auth/register");
 
 });
+
+router.post("/register", async (req,res)=>{
+    const userData = req.body;
+   
+    await authService.register(userData);
+
+    res.redirect("/login")
+})
 
 router.get("/login", (req,res)=>{
 res.render("auth/login");
